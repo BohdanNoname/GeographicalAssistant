c9942468dce79e0ccff491aa361a182120078475
BohdanNoname <nedashkovskiy1319@gmail.com>
2021-04-15 12:39:18 +0300

    com

--- app/src/main/java/com/nedashkivskyi/geographicalassistant/ui/fragments/data_countries/FragmentDataCountry.kt
+++ app/src/main/java/com/nedashkivskyi/geographicalassistant/ui/fragments/data_countries/FragmentDataCountry.kt
@@ -7,9 +7,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.nedashkivskyi.geographicalassistant.R
 import com.nedashkivskyi.geographicalassistant.databinding.FragmentDataCountryBinding
 import com.nedashkivskyi.geographicalassistant.ui.SharedViewModel
 import com.nedashkivskyi.geographicalassistant.ui.activities.CountryActivity
+import com.squareup.picasso.Picasso
+import java.io.File
+import java.util.*
 import javax.inject.Inject
 
 
@@ -18,20 +22,18 @@ class FragmentDataCountry: Fragment() {
     private lateinit var binding: FragmentDataCountryBinding
     @Inject lateinit var viewModel: SharedViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?): View {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
 
         (activity as CountryActivity).countryComponent.inject(this)
-        binding = FragmentDataCountryBinding
-            .inflate(inflater, container, false)
-        viewModel = ViewModelProvider(activity as CountryActivity)
-            .get(SharedViewModel::class.java)
+        binding = FragmentDataCountryBinding.inflate(inflater, container, false)
+        viewModel = ViewModelProvider(activity as CountryActivity).get(SharedViewModel::class.java)
 
         viewModel.country.observe(viewLifecycleOwner, Observer {
             binding.viewModel = viewModel
-//            (binding.emoji as ImageView).load(File("src/main/res/drawable/flags/ad.png"))
+            val code = it?.country()?.code().toString().toLowerCase(Locale.ROOT)
+            val url = "https://maxmaracakes.com.ua/my_flags_images/$code.png"
+
+            Picasso.get().load(url).into(binding.flag)
         })
         return binding.root
     }
