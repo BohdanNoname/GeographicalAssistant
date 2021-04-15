4735a2f12b56c6ba82e19fa483a22eeffe961255
BohdanNoname <nedashkovskiy1319@gmail.com>
2021-04-15 18:54:06 +0300

    com

--- app/src/main/java/com/nedashkivskyi/geographicalassistant/ui/fragments/data_countries/FragmentDataCountry.kt
+++ app/src/main/java/com/nedashkivskyi/geographicalassistant/ui/fragments/data_countries/FragmentDataCountry.kt
@@ -1,5 +1,6 @@
 package com.nedashkivskyi.geographicalassistant.ui.fragments.data_countries
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.nedashkivskyi.geographicalassistant.CountryQuery
 import com.nedashkivskyi.geographicalassistant.R
 import com.nedashkivskyi.geographicalassistant.databinding.FragmentDataCountryBinding
 import com.nedashkivskyi.geographicalassistant.ui.SharedViewModel
@@ -29,13 +31,27 @@ class FragmentDataCountry: Fragment() {
         viewModel = ViewModelProvider(activity as CountryActivity).get(SharedViewModel::class.java)
 
         viewModel.country.observe(viewLifecycleOwner, Observer {
-            binding.viewModel = viewModel
-            
-            val code = it?.country()?.code().toString().toLowerCase(Locale.ROOT)
-            val url = "https://maxmaracakes.com.ua/my_flags_images/$code.png"
-            Picasso.get().load(url).into(binding.flag)
+            uiRealization(it)
         })
 
         return binding.root
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun uiRealization(it: CountryQuery.Data?){
+        binding.viewModel = viewModel
+        val country = viewModel._country.value?.country()!!
+        val code = it?.country()?.code()!!
+        binding.namePlusCode.text = country.name() + " - " + code
+
+        val url = "https://maxmaracakes.com.ua/my_flags_images/${code.toLowerCase(Locale.ROOT)}.png"
+        Picasso.get().load(url).into(binding.flag)
+
+        val languages = arrayListOf<String>()
+        for (language in country.languages()){
+            languages.add(language.name().toString())
+        }
+
+        binding.languagesList.text = languages.toString().replace("[", "").replace("]", "")
     }
 }
